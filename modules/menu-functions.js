d862e1509aa7ea4f92db068acfd9eaaf37b3f6ea
Uchotski <smith.uchotski@gmail.com>
2022-03-30 22:58:10 +0100

    Added notes to the modules. Placed generateHTML within setGameArea to become a local function.

--- modules/menu-functions.js
+++ modules/menu-functions.js
@@ -2,14 +2,17 @@
 import { defaultCols, defaultRows } from './default-values.js';
 
 //MENU FUNCTIONS:
+//Open menu:
 const openMenu = () => {
     return document.getElementById("overlay").style.display = "flex";
 }
 
+//Close menu:
 const closeMenu = () => {
     return document.getElementById("overlay").style.display = "none";
 }
 
+//Reset the parameters within the game options:
 const resetMenu = () => {
     document.getElementById("word-length").value = defaultCols;
     return document.getElementById("turns").value = defaultRows;
--- modules/page-setup.js
+++ modules/page-setup.js
@@ -24,30 +24,31 @@ const setKeyboard = () => {
 }
 
 //GAME LAYOUT:
-const generateHTML = (rows, columns) => {
-    const htmlToAdd = [];
-
-    for (let i = 0; i < rows; i++) {
-        htmlToAdd.push(`<div class="row">`);
-        for (let j = 0; j < columns + 1; j++) {
-            if (j == 0) {
-                htmlToAdd.push(`  <div>${i + 1}</div>`);
-            } else {
-                htmlToAdd.push(`  <div class="game-tile"></div>`);
-            }
-        }
-        htmlToAdd.push(`</div>`);
-    }
-
-    return htmlToAdd.join("\n");
-}
-
+//Generates the HTML game tiles when the word length/number of guesses is changed.
 const setGameArea = () => {
     const rows = parseInt(document.getElementById("turns").value, 10);
     const columns = parseInt(document.getElementById("word-length").value, 10);
 
     if (rows > maxRows || rows < minRows || columns > maxCols || columns < minCols) {
         return alert(`Error! Number of turns must a value between ${minRows} and ${maxRows}, and word length must be between ${minCols} and ${maxCols}!`);
+    }
+
+    const generateHTML = (rows, columns) => {
+        const htmlToAdd = [];
+    
+        for (let i = 0; i < rows; i++) {
+            htmlToAdd.push(`<div class="row">`);
+            for (let j = 0; j < columns + 1; j++) {
+                if (j == 0) {
+                    htmlToAdd.push(`  <div>${i + 1}</div>`);
+                } else {
+                    htmlToAdd.push(`  <div class="game-tile"></div>`);
+                }
+            }
+            htmlToAdd.push(`</div>`);
+        }
+    
+        return htmlToAdd.join("\n");
     }
 
     closeMenu();
