fac51004916b3b835a1f858c9eea1ab68e8da2f0
Uchotski <smith.uchotski@gmail.com>
2022-03-30 22:45:31 +0100

    Added incomplete script to shrink tiles on resize.

--- script.js
+++ script.js
@@ -8,25 +8,37 @@ setLimits(minCols, maxCols, defaultCols, minRows, maxRows, defaultRows);
 setGameArea(defaultRows, defaultCols);
 setKeyboard();
 
-<<<<<<< HEAD
-//ADD EVENT LISTENERS:
-=======
-//GENERATE A RANDOM WORD:
+//TEST ZONE:
+//Script seems to work for shrinking but, not enlarging...
+const setGameTileSize = () => {
+    const columns = document.getElementById("word-length").value;
+    const keyboard = document.getElementById("keyboard");
+    const gameArea = document.getElementById("game-area");
+    const gameRows = gameArea.querySelectorAll("div.row");
+    const gameRowNumber = gameArea.querySelectorAll("div:not(.row):not(.game-tile)");
+    const gameTiles = gameArea.querySelectorAll("div.game-tile");
 
-const generateWord = (num) => {
-    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const wordToGuess = [];
+    const changeTileSize = (wordLength) => {
+        const tileMargin = parseInt(window.getComputedStyle(gameArea.querySelector("div.game-tile")).getPropertyValue("margin"), 10);
+        const newTileSize = Math.floor((gameRows[0].offsetWidth / wordLength) - ((2 * gameRowNumber[0].offsetWidth) / wordLength)) - (tileMargin * 2);
+        console.log(newTileSize);
 
-    for (let i = 0; i < num; i++) {
-        wordToGuess.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+        gameTiles.forEach(tile => {
+            tile.style.width = `${newTileSize - 10}px`;
+            tile.style.height = `${newTileSize - 10}px`;
+        });
+        gameRowNumber.forEach(num => { num.style.height = `${newTileSize - 10}px` });
     }
 
-    return wordToGuess;
+    if (gameArea.offsetWidth + 10 > keyboard.offsetWidth) {
+        return changeTileSize(columns);
+    }
 }
 
-//EVENT LISTENERS:
->>>>>>> 7077239de384a75497bd55ca505d843c11f1d68b
+
+//ADD EVENT LISTENERS:
 document.getElementById("menu-button").addEventListener("click", openMenu);
 document.getElementById("close-button").addEventListener("click", closeMenu);
 document.getElementById("start-button").addEventListener("click", setGameArea);
 document.getElementById("reset-button").addEventListener("click", resetMenu);
+window.addEventListener("resize", setGameTileSize);
